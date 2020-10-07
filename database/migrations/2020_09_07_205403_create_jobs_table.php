1be7b6968ad244d25c5069a1fb7c392df3608495
Jamine29 <s73092@beuth-hochschule.de>
2020-10-07 23:10:15 +0200

    Added Create Table for Companies, Jobs, CompanyUsers

---
+++ database/migrations/2020_09_07_205403_create_jobs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 150);
+            $table->string('description', 250);
+            $table->bigInteger('companyId')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('companyId')
+                    ->references('id')
+                    ->on('companies')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+}
---
+++ database/migrations/2020_10_07_194823_create_company_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompanyUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company_users', function (Blueprint $table) {
+            $table->bigInteger('userId')->unsigned();
+            $table->bigInteger('companyId')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('userId')
+                    ->references('id')
+                    ->on('users')
+                    ->onDelete('cascade');
+
+            $table->foreign('companyId')
+                    ->references('id')
+                    ->on('companies')
+                    ->onDelete('cascade');
+
+            $table->primary(array('companyId', 'userId'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('companies');
+        Schema::dropIfExists('company_users');
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+    }
+}
